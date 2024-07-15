92daec00d5aa26b8148f58cf9a8bff5a00ca06fa
joperopi <70579397+joperopi@users.noreply.github.com>
2024-07-15 19:26:31 -0300

    Add round play function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 console.log("RECON CRAFT(JS File) CHECKING FOR CONNECTION (Console.logging) WITH MOTHERSHIP (HTML File)")
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let computerChoice = "";
     randomNum = Math.random()
@@ -19,4 +22,18 @@ function getHumanChoice() {
         humanChoice = "rock";
     }
     return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`You and the computer chose ${humanChoice}! It's a tie!`);
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")) {
+        console.log(`${humanChoice} beats ${computerChoice}! You win!`);
+        return humanScore++;
+    } else {
+        console.log(`${computerChoice} beats ${humanChoice}! You lose!`);
+        return computerScore++;
+    }
 }
